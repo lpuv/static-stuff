b7230e9b1e364a3614e8d3290ef8675cd05318e6
#!/bin/cat <42851999+gamer4life1@users.noreply.github.com>
2021-12-01 14:00:15 -0800

    dont install from git

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ pip3 install --upgrade pip
 pip3 install --upgrade distlib
 python3.9 get-pip.py
 rm get-pip.py
-python3.9 -m pip install git+https://github.com/calebstewart/pwncat.git
-python3.9 -m pip install -U git+https://github.com/calebstewart/paramiko
-python3.9 -m pip install git+https://github.com/JohnHammond/base64io-python
+python3.9 -m pip install pwncat-cs
+# python3.9 -m pip install -U git+https://github.com/calebstewart/paramiko
+# python3.9 -m pip install git+https://github.com/JohnHammond/base64io-python
 wget https://github.com/21y4d/nmapAutomator/raw/master/nmapAutomator.sh
 chmod +x nmapAutomator.sh
 git clone https://github.com/gamer4life1/poor-mans-pentest /opt/pmp
