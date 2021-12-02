b15763258b93fafa164e171f1108c2eb8de5cf88
#!/bin/cat <42851999+gamer4life1@users.noreply.github.com>
2021-12-02 10:24:21 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
+set -x
 add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.9 python3.9-dev python3-pip python3.9-distutils ruby ruby-dev xautomation guake
 wget https://bootstrap.pypa.io/get-pip.py
-pip3 install --upgrade setuptools
-pip3 install --upgrade pip
-pip3 install --upgrade distlib
+# pip3 install --upgrade setuptools
+# pip3 install --upgrade pip
+# pip3 install --upgrade distlib
 python3.9 get-pip.py
 rm get-pip.py
 python3.9 -m pip install -U setuptools pip distlib
