b465d548fb174b7a0bc0a3591633b4af8696037f
Leo Puvilland <leo@craftcat.dev>
2022-05-10 16:38:29 -0700

    change OU

--- ad.sh
+++ ad.sh
@@ -5,7 +5,7 @@ echo "set rdns=false and fix krb5.conf"
 read  -n 1 -p "ready?" mainmenuinput
 apt install policykit-1 realmd sssd sssd-tools libnss-sss libpam-sss adcli
 
-realm join -v -U 'leo' --os-name="`uname -o`" --os-version="`uname -rsv`" INTERNAL.LAB --install=/
+realm join -v -U 'leo' --os-name="`uname -o`" --os-version="`uname -rsv`" INTERNAL.LAB --install=/ --computer-ou="CN=Servers,DC=internal,DC=lab"
 
 cat <<EOT >> /etc/sssd/sssd.conf
 dyndns_update = true
