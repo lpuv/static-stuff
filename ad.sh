3797bf02621d55aa22ab288ce4e5819bde39d81c
Leo <leo@craftcat.dev>
2022-05-10 14:50:10 -0700

    sssd

--- ad.sh
+++ ad.sh
@@ -6,3 +6,12 @@ wait 10
 apt install policykit-1 realmd sssd sssd-tools libnss-sss libpam-sss adcli
 
 realm join -v -U 'leo' --os-name="`uname -o`" --os-version="`uname -rsv`" INTERNAL.LAB --install=/
+
+cat <<EOT >> /etc/sssd/sssd.conf
+dyndns_update = true
+dyndns_refresh_interval = 43200
+dyndns_update_ptr = true
+dyndns_ttl = 3600
+EOT
+
+systemctl restart sssd
