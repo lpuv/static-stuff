13ead1e8250ae2f9452b8e50581aacb80844af51
Leo <leo@craftcat.dev>
2022-05-10 14:52:38 -0700

    more stuff

--- ad.sh
+++ ad.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "set hostname and /etc/hosts file..."
-wait 10
+read  -n 1 -p "ready?" mainmenuinput
 echo "set rdns=false and fix krb5.conf"
-wait 10
+read  -n 1 -p "ready?" mainmenuinput
 apt install policykit-1 realmd sssd sssd-tools libnss-sss libpam-sss adcli
 
 realm join -v -U 'leo' --os-name="`uname -o`" --os-version="`uname -rsv`" INTERNAL.LAB --install=/
